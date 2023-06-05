c59a1e7cbbdd17ebbf4253dbd1bbc1736c51c60b
kuropenguin <kuro1994.t@gmail.com>
2023-06-06 07:21:08 +0900

    中身実装

--- 605_can_place_flowers.py
+++ 605_can_place_flowers.py
@@ -3,6 +3,21 @@ from typing import List
 
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        for idx in range(len(flowerbed)):
+            if idx == 0:
+                if flowerbed[idx] == 0 and flowerbed[idx + 1] == 0:
+                    flowerbed[idx] = 1
+                    n -= 1
+            elif idx == len(flowerbed) - 1:
+                if flowerbed[idx] == 0 and flowerbed[idx - 1] == 0:
+                    flowerbed[idx] = 1
+                    n -= 1
+            else:
+                if flowerbed[idx] == 0 and flowerbed[idx - 1] == 0 and flowerbed[idx + 1] == 0:
+                    flowerbed[idx] = 1
+                    n -= 1
+            if n == 0:
+                return True
         return False
 
 
@@ -11,12 +26,12 @@ solution = Solution()
 flowerbed = [1, 0, 0, 0, 1]
 n = 1
 excepted = True
-result = solution.kidsWithCandies(flowerbed=flowerbed, n=n)
-print(result)
+result = solution.canPlaceFlowers(flowerbed=flowerbed, n=n)
+print(result == excepted)
 
 
 flowerbed = [1, 0, 0, 0, 1]
 n = 2
 excepted = False
-result = solution.kidsWithCandies(flowerbed=flowerbed, n=n)
-print(result)
+result = solution.canPlaceFlowers(flowerbed=flowerbed, n=n)
+print(result == excepted)
