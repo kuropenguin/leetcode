198ccf5dbeaea2237b9b34939379f49d1362b99b
kuropenguin <kuro1994.t@gmail.com>
2023-06-13 07:22:37 +0900

    init 345

---
+++ 345_reverse_vowels_of_a_string.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        return ""
+
+
+solution = Solution()
+
+s = "hello"
+excepted = "holle"
+result = solution.reverseVowels(s=s)
+print(result)
+print(result == excepted)
