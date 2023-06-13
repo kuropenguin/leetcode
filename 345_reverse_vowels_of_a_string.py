883ea4a35fb4bf0d2c98cfdc7a75b25a38cfdfc5
kuropenguin <kuro1994.t@gmail.com>
2023-06-14 07:10:52 +0900

    fix 345

--- 345_reverse_vowels_of_a_string.py
+++ 345_reverse_vowels_of_a_string.py
@@ -1,18 +1,31 @@
+from collections import deque
 class Solution:
-    def reverseVowels(self, s: str) -> str:
-        return ""
+    def reverseVowels(self, string: str) -> str:
+        idx_stack = []
+        str_queue = deque()
+        str_list = list(string)
+        for idx in range(len(string)):
+            if string[idx] in "aeiouAEIOU":
+                idx_stack.append(idx)
+                str_queue.append(string[idx])
+        while idx_stack:
+            idx = idx_stack.pop()
+            s = str_queue.popleft()
+            str_list[idx] = s
+
+        return "".join(str_list)
 
 
 solution = Solution()
 
 s = "hello"
 excepted = "holle"
-result = solution.reverseVowels(s=s)
+result = solution.reverseVowels(string=s)
 print(result)
 print(result == excepted)
 
 s = "leetcode"
 expect = "leotcede"
-result = solution.reverseVowels(s=s)
+result = solution.reverseVowels(string=s)
 print(result)
 print(result == expect)
