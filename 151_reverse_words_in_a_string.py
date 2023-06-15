8e809e90c0e04cc7f4e750fbc1efa7f5c7ee194a
kuropenguin <kuro1994.t@gmail.com>
2023-06-16 07:20:44 +0900

    一旦完成

--- 151_reverse_words_in_a_string.py
+++ 151_reverse_words_in_a_string.py
@@ -1,33 +1,40 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        str_list = list(string)
+        str_list = []
         for idx in range(len(s)):
-            if s[idx] != " ":
-                str_list.append(s[idx])
+            str_list.append(s[idx])
+        print(str_list)
         result = ""
         word = ""
         for idx in range(len(str_list)):
-            str = str_list.pop()
-            if str == " ":
-                word = reverse(word)
+            char = str_list.pop()
+            if char == " ":
+                word = word[::-1]
                 if word != "":
-                    result += " " + word
+                    result += " " + word if result != "" else word
                 word = ""
             else:
-                word += str
-        return " ".join(s.split()[::-1])
+                word += char
+        if result == "":
+            result += word[::-1]
+        else:
+            if word != "":
+                result += " " + word[::-1]
+        return result
 
 
 solution = Solution()
 
 s = "the sky is blue"
-excepted = "blue is sky the"
+expected = "blue is sky the"
 result = solution.reverseWords(s=s)
 print(result)
-print(result == excepted)
+print(expected)
+print(result == expected)
 
 s = "  hello world  "
 expected = "world hello"
 result = solution.reverseWords(s=s)
 print(result)
-print(result == excepted)
+print(expected)
+print(result == expected)
