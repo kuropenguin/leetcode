4175495b41bf5a267d24c6077972231c80aa5afa
kuropenguin <kuro1994.t@gmail.com>
2023-07-20 07:11:41 +0900

    init 283!

---
+++ leetcode75/283_move_zeros.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+
+solution = Solution()
+
+nums = [0, 1, 0, 3, 12]
+excepted = [1, 3, 12, 0, 0]
+result = solution.moveZeroes(nums=nums)
+print(result)
