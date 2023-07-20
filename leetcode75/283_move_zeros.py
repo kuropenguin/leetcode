be9b545a98fd46ccb35ada007f4cacdcd61fb17a
kuropenguin <kuro1994.t@gmail.com>
2023-07-21 06:53:19 +0900

    283 sol

--- leetcode75/283_move_zeros.py
+++ leetcode75/283_move_zeros.py
@@ -6,6 +6,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        for idx in range(0, len(nums)):
+            if nums[idx] == 0:
+                for jdx in range(idx + 1, len(nums)):
+                    if nums[jdx] != 0:
+                        nums[idx], nums[jdx] = nums[jdx], nums[idx]
+                        break
 
 
 solution = Solution()
@@ -13,4 +19,13 @@ solution = Solution()
 nums = [0, 1, 0, 3, 12]
 excepted = [1, 3, 12, 0, 0]
 solution.moveZeroes(nums=nums)
+print(nums)
+print(excepted)
+print(nums == excepted)
+
+nums = [0]
+excepted = [0]
+solution.moveZeroes(nums=nums)
+print(nums)
+print(excepted)
 print(nums == excepted)
