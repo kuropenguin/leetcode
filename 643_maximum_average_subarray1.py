03b9609334559cf258811a34aecb1ae16856b4c2
kuropenguin <kuro1994.t@gmail.com>
2023-07-27 07:13:50 +0900

    timeout soltion

--- 643_maximum_average_subarray1.py
+++ 643_maximum_average_subarray1.py
@@ -3,7 +3,10 @@ from typing import List
 
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        return 1
+        max_sum = -float('inf')
+        for i in range(0, len(nums) - k + 1):
+            max_sum = max(max_sum, sum(nums[i:i+k]))
+        return max_sum / k
 
 
 solution = Solution()
@@ -11,6 +14,7 @@ solution = Solution()
 nums = [1, 12, -5, -6, 50, 3]
 k = 4
 result = solution.findMaxAverage(nums=nums, k=k)
+print(result)
 print(result == 12.75000)
 
 nums = [5]
