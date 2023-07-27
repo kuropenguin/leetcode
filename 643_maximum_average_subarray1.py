912e29a7c19bcc3e130a2794c022d58595bdb5bd
kuropenguin <kuro1994.t@gmail.com>
2023-07-28 07:12:02 +0900

    fix 643

--- 643_maximum_average_subarray1.py
+++ 643_maximum_average_subarray1.py
@@ -3,9 +3,10 @@ from typing import List
 
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        max_sum = -float('inf')
-        for i in range(0, len(nums) - k + 1):
-            max_sum = max(max_sum, sum(nums[i:i+k]))
+        max_sum = current_sum = sum(nums[0:k])
+        for i in range(1, len(nums) - k + 1):
+            current_sum = current_sum - nums[i - 1] + nums[i + k - 1]
+            max_sum = max(max_sum, current_sum)
         return max_sum / k
 
 
