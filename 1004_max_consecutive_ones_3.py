947ce7072ca7d24c3623d87e5abf11c0aba0958b
kuropenguin <kuro1994.t@gmail.com>
2023-08-23 07:11:54 +0900

    fix 1004

--- 1004_max_consecutive_ones_3.py
+++ 1004_max_consecutive_ones_3.py
@@ -1,48 +1,23 @@
-from typing import List
+from collections import deque
 
 
 class Solution:
-    def longestOnes(self, nums: List[int], k: int) -> int:
-        maximun = 0
-        for i in range(len(nums)):
-            current_k = k
-            count = 0
-            if nums[i] == 0:
-                current_k -= 1
-                if current_k >= 0:
-                    count += 1
-                    maximun = max(maximun, count)
-                else:
-                    continue
-            else:
-                count += 1
-            for j in range(i+1, len(nums)):
-                if nums[j] == 0:
-                    current_k -= 1
-                    if current_k >= 0:
-                        count += 1
-                        maximun = max(maximun, count)
-                    else:
-                        break
-                else:
-                    count += 1
-                    maximun = max(maximun, count)
+    def longestOnes(self, nums, k):
 
-        return maximun
+        left = 0
+        res = 0
+        # We will store the index of 0 in this deque
+        deq = deque()
 
+        for right, num in enumerate(nums):
+            if num == 0:
+                deq.append(right)
+            # When the deque hits the maximum we popleft to remove first zero we countered
+            # Similarly we update the left window
+            while len(deq) > k:
+                idx = deq.popleft()
+                left = idx + 1
 
-solution = Solution()
+            res = max(res, right - left + 1)
 
-nums = [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0]
-k = 2
-expected = [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1]
-result = solution.longestOnes(nums=nums, k=k)
-print(result)
-print(result == 6)
-
-nums = [0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1]
-k = 3
-expected = [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1]
-result = solution.longestOnes(nums=nums, k=k)
-print(result)
-print(result == 10)
+        return res
