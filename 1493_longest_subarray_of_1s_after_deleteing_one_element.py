01ee4cb9fd16a1ca45fa17c552126e877de9af00
kuropenguin <kuro1994.t@gmail.com>
2023-09-13 07:05:02 +0900

    fix 1493

--- 1493_longest_subarray_of_1s_after_deleteing_one_element.py
+++ 1493_longest_subarray_of_1s_after_deleteing_one_element.py
@@ -3,7 +3,28 @@ from typing import List
 
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        return 1
+        if len(nums) == 1:
+            return 0
+        left = 0
+        zero_point = 0
+        right = 1
+        zero_count = 0
+        if nums[0] == 0:
+            zero_count += 1
+        max_count = 0
+        while right < len(nums):
+            if nums[right] == 0:
+                zero_count += 1
+                if zero_count == 2:
+                    max_count = max(max_count, right - left - 1)
+                    left = zero_point + 1
+                    zero_point = right
+                    zero_count -= 1
+                else:
+                    zero_point = right
+            max_count = max(max_count, right - left - 1)
+            right += 1
+        return max(max_count, right - left - 1)
 
 
 solution = Solution()
@@ -19,7 +40,18 @@ result = solution.longestSubarray(nums=nums)
 print(result)
 print(result == 5)
 
-nums =[1,1,1]
+nums = [1, 1, 1]
 result = solution.longestSubarray(nums=nums)
 print(result)
 print(result == 2)
+
+
+nums = [1, 1, 0, 0, 1, 1, 1, 0, 1]
+result = solution.longestSubarray(nums=nums)
+print(result)
+print(result == 4)
+
+nums = [0, 1, 1, 1, 0, 1, 1, 0, 1]
+result = solution.longestSubarray(nums=nums)
+print(result)
+print(result == 5)
